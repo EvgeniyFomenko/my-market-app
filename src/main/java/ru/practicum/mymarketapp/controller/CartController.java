23579aa22df3ced2b7eb2932b24b21c23b0c2b55
evgeny fomenko <efomenkoone@yandex.ru>
2026-03-26 17:14:12 +0500

    ref обновил ченж лог

--- src/main/java/ru/practicum/mymarketapp/controller/CartController.java
+++ src/main/java/ru/practicum/mymarketapp/controller/CartController.java
@@ -1,22 +1,23 @@
 package ru.practicum.mymarketapp.controller;
 
-import jakarta.transaction.Transactional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.practicum.mymarketapp.entity.CartItemCount;
-import ru.practicum.mymarketapp.entity.Item;
-import ru.practicum.mymarketapp.entity.Order;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.practicum.mymarketapp.entity.dto.ItemDto;
 import ru.practicum.mymarketapp.entity.dto.ItemDtoConverter;
+import ru.practicum.mymarketapp.pojo.FormData;
 import ru.practicum.mymarketapp.service.CartItemCountService;
 import ru.practicum.mymarketapp.service.ItemService;
 import ru.practicum.mymarketapp.service.OrderService;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -32,32 +33,45 @@ public class CartController {
     }
 
     @GetMapping("/cart/items")
-    public String cartItems(Model model) {
-        Order order = orderService.findNewOrder();
+    public Mono<String> cartItems(Model model) {
+        return orderService.findNewOrderOrTakeNew().doOnNext(order -> model.addAttribute("total", order.getTotal()))
+                .map(order -> cartItemCountService.findItemByOrderId(order.getId()))
+                .flatMap(Flux::collectList)
+                .map(e -> e.stream().map(ItemDtoConverter::toDto)
+                        .collect(Collectors.toList())).map(e ->
+                        {
+                            model.addAttribute("items", e);
+                            return "cart";
+                        }
+                ).switchIfEmpty(getEmptyCart(model));
+    }
 
-        List<ItemDto> items = cartItemCountService.findItemByOrderId(order).stream()
-                .map(ItemDtoConverter::toDto).toList();
-        model.addAttribute("items", items);
-        model.addAttribute("total", Objects.isNull(order)? 0 : order.getTotal());
-
-        return "cart";
+    private Mono<String> getEmptyCart(Model model) {
+        model.addAllAttributes(Map.of("items", new ArrayList<ItemDto>(), "total", 0));
+        return Mono.just("cart");
     }
 
     @PostMapping("/cart/items")
     @Transactional
-    public String cartItemsAction(Model model, @RequestParam Long id, @RequestParam String action) {
-        Order order = orderService.findNewOrder();
-        Item item = itemService.findById(id);
-        CartItemCount cartItemCount = cartItemCountService.createOrFindByOrderAndItemId(order,item);
-        cartItemCountService.changePriceCartByAction(cartItemCount,action);
-        orderService.changePriceOrderByActionOnCartItemCount(order,action,cartItemCount);
+    public Mono<String> cartItemsAction(Model model, @ModelAttribute FormData formData) {
+        Long id = Long.parseLong(formData.getId());
+        String action = formData.getAction();
+        return orderService.findNewOrderOrTakeNew().log()
+                .flatMap(order -> cartItemCountService.createOrFindByOrderAndItemId(order.getId(),id))
+                .flatMap(cartItemCount1 -> cartItemCountService.changePriceCartByAction(cartItemCount1, action))
+                .flatMap(e -> orderService.changePriceOrderByActionOnCartItemCount(action, e))
+                .flatMap(e -> {
+                    model.addAttribute("total", e.getTotal());
+                    return cartItemCountService.findItemByOrderId(e.getId()).map(ItemDtoConverter::toDto).collectList();
+                }).map(e -> {
+                    model.addAttribute("items", e);
+                    return "cart";
+                }).switchIfEmpty(Mono.just("cart")
+                        .doOnNext(e -> {
+                            model.addAttribute("items", Collections.emptyList());
+                            model.addAttribute("total", 0);
+                        }));
 
-        List<ItemDto> items = cartItemCountService.findItemByOrderId(order).stream()
-                .map(ItemDtoConverter::toDto).toList();
-        model.addAttribute("items", items);
-        model.addAttribute("total", Objects.isNull(order)? 0 : order.getTotal());
 
-        return "cart";
     }
-
 }
--- src/main/java/ru/practicum/mymarketapp/entity/CartItemCount.java
+++ src/main/java/ru/practicum/mymarketapp/entity/CartItemCount.java
@@ -1,38 +1,32 @@
 package ru.practicum.mymarketapp.entity;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
-
-@Entity
-@Table
+@Table(name="cart_item_count")
+//@Entity
 public class CartItemCount {//Количество вещей в карзине
-    @Column
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "generated_counter")
-    @SequenceGenerator(allocationSize = 1, name = "generated_counter")
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Order orderId;
-    @ManyToOne
-    @JoinColumn(name = "item_id")
-    private Item itemId;
+    private Long orderId;
+    private Long itemId;
     //Количество товаров в корзине/заказе
     private int quantity;
 
-    public Order getOrderId() {
+    public Long getOrderId() {
         return orderId;
     }
 
-    public void setOrderId(Order orderId) {
+    public void setOrderId(Long orderId) {
         this.orderId = orderId;
     }
 
-    public Item getItemId() {
+    public Long getItemId() {
         return itemId;
     }
 
-    public void setItemId(Item itemId) {
+    public void setItemId(Long itemId) {
         this.itemId = itemId;
     }
 
@@ -50,5 +44,15 @@ public class CartItemCount {//Количество вещей в карзине
 
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItemCount{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", itemId=" + itemId +
+                ", quantity=" + quantity +
+                '}';
     }
 }
