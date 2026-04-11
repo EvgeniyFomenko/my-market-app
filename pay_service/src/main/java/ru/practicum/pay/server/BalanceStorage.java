565e6dc0bbbb81df35021bd781bcdc61c786141a
evgeny fomenko <efomenkoone@yandex.ru>
2026-04-11 23:26:05 +0500

    feature добавил pay service

---
+++ pay_service/src/main/java/ru/practicum/pay/server/BalanceStorage.java
@@ -0,0 +1,22 @@
+package ru.practicum.pay.server;
+
+import org.springframework.stereotype.Component;
+
+
+import java.math.BigDecimal;
+@Component
+public class BalanceStorage {
+    public BalanceStorage() {
+
+        balance = BigDecimal.valueOf((int)(Math.random() * ((1000 - 200) + 1)) + 200);
+    }
+
+    private BigDecimal balance;
+    public String getBalance() {
+        return balance.toString();
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
