1295a4bcfd7eb36c299ec1bcee9519adbace94cd
evgeny fomenko <efomenkoone@yandex.ru>
2026-06-08 21:03:02 +0500

    feature произвел необходимые настройки security pay

--- pay_service/src/main/java/ru/practicum/pay/server/PayController.java
+++ pay_service/src/main/java/ru/practicum/pay/server/PayController.java
@@ -14,43 +14,44 @@ import java.util.Optional;
 
 
 @Controller
-public class PayController implements DefaultApi {
-    public PayController(PayService payService) {
+public class PayController extends DefaultApi {
+    public PayController(PayService payService, ServerWebExchange exchange) {
         this.payService = payService;
+        this.exchange = exchange;
     }
 
     private final PayService payService;
+    private final ServerWebExchange exchange;
 
-    @Override
-   public Mono<ResponseEntity<Balance>> toPayPost(
-            @Parameter(name = "Quantity", description = "стоимость вещи", required = true) @Valid @RequestBody Mono<Quantity> quantity,
+   public Mono<Balance> toPayPost(
+            @Parameter(name = "Quantity", description = "стоимость вещи", required = true) @Valid @RequestBody Quantity quantity,
             @Parameter(hidden = true) final ServerWebExchange exchange
     ) {
-       return quantity.doOnNext(e -> payService.changeBalance(e.getQuantity())).flatMap(
+       return Mono.just(quantity).doOnNext(e -> payService.changeBalance(e.getQuantity())).flatMap(
                 e -> {
                     Balance balance = new Balance();
                     balance.setBalance(payService.getBalance());
-                   return Mono.just(ResponseEntity.of(Optional.of(balance)));
+                   return Mono.just(balance);
                 }
         );
     }
 
-    @Override
-    public Mono<ResponseEntity<Balance>> getBalanceGet(
-            @Parameter(hidden = true) final ServerWebExchange exchange
-    ) {
-        Mono<Void> result = Mono.empty();
+    public Mono<Balance> getBalanceGet( ) {
+//        Mono<Void> result = Mono.empty();
 
-        exchange.getResponse().setStatusCode(HttpStatus.valueOf(200));
-        for (MediaType mediaType : exchange.getRequest().getHeaders().getAccept()) {
-            if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
-                String exampleString = "{ \"balance\" : \""+payService.getBalance()+"\" }";
-                result = ApiUtil.getExampleResponse(exchange, MediaType.valueOf("application/json"), exampleString);
-                break;
-            }
-        }
+//        exchange.getResponse().setStatusCode(HttpStatus.valueOf(200));
+//        for (MediaType mediaType : exchange.getRequest().getHeaders().getAccept()) {
+//            if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+//                String exampleString = "{ \"balance\" : \""+payService.getBalance()+"\" }";
+//                result = ApiUtil.getExampleResponse(exchange, MediaType.valueOf("application/json"), exampleString);
+//                break;
+                Balance balance = new ru.practicum.pay.server.Balance();
+                balance.setBalance(payService.getBalance());
+                return Mono.just(balance);
+//            }
+//        }
 
 
-        return result.then(Mono.empty());
+//        return result.then(Mono.empty());
     }
 }
